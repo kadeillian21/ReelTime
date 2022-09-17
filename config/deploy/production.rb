756cf906ff286f32e0cecea98835fd5c92785f2b
Kade Illian <kadeillianmt@gmail.com>
2022-09-17 11:09:03 -0600

    added server syntax to production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
+server '217.21.78.116', user: 'deploy', roles: %w{app db web}
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value

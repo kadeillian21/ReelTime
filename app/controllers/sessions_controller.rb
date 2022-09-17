7992d87e05d24bfa14fe198c44f56fca78044b45
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-17 14:35:13 -0700

    Removes email from login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email]) || user = User.find_by(user_name: params[:user_name])
+    user = User.find_by(user_name: params[:user_name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to "/"

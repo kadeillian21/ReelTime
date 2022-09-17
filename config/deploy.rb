7b572e060b84808485d61a2175e4fd09fe2b8adf
Kade Illian <kadeillianmt@gmail.com>
2022-09-17 13:12:15 -0600

    updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "reeltime"
+set :repo_url, "https://github.com/rails-hackathon/team-9.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp

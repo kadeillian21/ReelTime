66bde8466a23711d491d0ea8947f91ab46490a08
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-17 12:12:22 -0700

    Adds associations between users via relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :user_name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+  has_many :follower_relationships, class_name: "Relationship", foreign_key: "leader_id"
+  has_many :followers, through: :follower_relationships, source: :follower
+  has_many :leader_relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :leaders, through: :leader_relationships, source: :leader
 end

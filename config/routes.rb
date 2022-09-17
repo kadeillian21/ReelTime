009859b2122105f80650732bd712bde88c9e31cc
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-17 13:13:30 -0700

    Adds sessions routes and controller methods for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  get "/login" => "sessions#new"
   post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end

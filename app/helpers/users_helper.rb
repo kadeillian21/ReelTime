c05dceb398b5e953c9380bb18a9831fe13f60830
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-18 17:04:01 -0700

    Adds user image to _user partial

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  # def image_for(user)
+  #   @image = user.image
+  #   if @image.empty?
+  #     @image_user = image_tag("user.jpg", alt: user.full_name)
+  #   else
+  #     @image_user = image_tag(user.image_url, alt: user.full_name)
+  #   end
+  #   return @image_user
+  # end
 end

45e1a9492761ca7b8e132de7ea30965421ad4b5e
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-18 17:13:23 -0700

    Adds default user.jpg image upload if user doesn't add one when creating account

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
-  # def image_for(user)
-  #   @image = user.image
-  #   if @image.empty?
-  #     @image_user = image_tag("user.jpg", alt: user.full_name)
-  #   else
-  #     @image_user = image_tag(user.image_url, alt: user.full_name)
-  #   end
-  #   return @image_user
-  # end
+  def image_for(user, width = "", height = "")
+    @image = user.image
+    if @image == nil
+      @image_user = image_tag("user.jpg", alt: user.full_name, width: width, height: height)
+    else
+      @image_user = image_tag(user.image_url, alt: user.full_name, width: width, height: height)
+    end
+    return @image_user
+  end
 end

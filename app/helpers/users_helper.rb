0716f9c11b29206918a9ef10c5be7f16c1b0c083
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-18 18:00:22 -0700

    Adds destroy and edit user methods to helper to give access only to user that is logged in for their own user page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,16 @@ module UsersHelper
     end
     return @image_user
   end
+
+  def destroy_user(user)
+    if user == current_user
+      button_to "Destroy this user", user, method: :delete
+    end
+  end
+
+  def edit_user(user)
+    if user == current_user
+      link_to "Edit this user", edit_user_path(user)
+    end
+  end
 end

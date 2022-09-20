e1e513b7c7bc0132a72a3d60534e1fcf1afa20bd
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-20 06:58:46 -0700

    Adds image_for() method into OriginalPostsHelper

--- app/helpers/orignial_posts_helper.rb
+++ app/helpers/orignial_posts_helper.rb
@@ -1,4 +1,14 @@
 module OrignialPostsHelper
+  def image_for(user, width = "", height = "")
+    @image = original_post.image
+    if @image == nil
+      @image_original_post = image_tag("user.jpg", alt: "#{user.full_name}'s post #{original_post.id}", width: width, height: height)
+    else
+      @image_original_post = image_tag(original_post.image_url, alt: "#{user.full_name}'s post #{original_post.id}", width: width, height: height)
+    end
+    return @image_original_post
+  end
+
   def edit_original_post(original_post)
     if current_user&.id == original_post.user.id
       link_to "Edit this Post", edit_original_post_path(original_post)

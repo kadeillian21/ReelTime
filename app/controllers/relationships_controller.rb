205036ff13e4b44c84f03be1cacf6f3a834370d5
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-21 07:03:47 -0700

    Attempts to flesh out a functioning create action in relationships

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -7,11 +7,11 @@ class RelationshipsController < ApplicationController
   end
 
   def create
-    @relationship = Relationship.new(leader_id: params[:leader_id], follower_id: current_user.id)
+    @relationship = Relationship.new(leader_id: @user.id, follower_id: current_user.id)
 
     respond_to do |format|
       if @relationship.save
-        format.html { redirect_to relationship_url(@relationship), notice: "You followed this user." }
+        format.html { redirect_to user_url(@user), notice: "You followed this user." }
         format.json { render :show, status: :created, location: @relationship }
       end
     end

ba816b3935eae3c672b81157cf510c3d12bf29d7
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-21 06:52:06 -0700

    Adds new and create actions to relaitonships_controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,22 @@
 class RelationshipsController < ApplicationController
+  def show
+  end
+
+  def new
+    @relationship = Relationship.new
+  end
+
+  def create
+    @relationship = Relationship.new(leader_id: params[:leader_id], follower_id: current_user.id)
+
+    respond_to do |format|
+      if @relationship.save
+        format.html { redirect_to relationship_url(@relationship), notice: "You followed this user." }
+        format.json { render :show, status: :created, location: @relationship }
+      end
+    end
+  end
+
+  def destroy
+  end
 end

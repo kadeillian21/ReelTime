13e59854e74a3de36430499937dbe457d2bdde90
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-22 07:36:35 -0700

    Adds destroy action to relationships_controller and an unfollow button to users show page

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -12,12 +12,17 @@ class RelationshipsController < ApplicationController
 
     respond_to do |format|
       if @relationship.save
-        format.html { redirect_to relationship_url(@relationship), notice: "You followed this user." }
+        format.html { redirect_to relationship_url(@relationship), notice: "User followed." }
         format.json { render :show, status: :created, location: @relationship }
       end
     end
   end
 
   def destroy
+    @relationship = Relationship.find_by(leader_id: params[:leader_id], follower_id: current_user.id)
+    @relationship.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: "User was successfully unfollowed." }
+      format.json { head :no_content }
   end
 end

6e53115957ae92cb99e0216ff8e8e99d720e8cc9
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-24 10:19:49 -0700

    Adds post route to share_post partial, and attempts to call post_params model method from shared_posts_controller create method[BROKEN]

--- app/controllers/shared_posts_controller.rb
+++ app/controllers/shared_posts_controller.rb
@@ -6,18 +6,22 @@ class SharedPostsController < ApplicationController
   end
 
   def new
+    @shared_post = SharedPost.new
   end
 
   def create
-    @shared_post = SharedPost.new(post_params("https://www.cnet.com/science/space/stunning-nasa-james-webb-telescope-image-reveals-neptunes-delicate-rings/"))
+    # post_params = post_params(:url)
+    # @shared_post = SharedPost.new(url: post_params[:url], image_url: post_params[:image_url], title: post_params[:title], user_id: current_user.id)
+    @shared_post = SharedPost.new.post_params(params["shared_post"][:url])
     respond_to do |format|
       if @shared_post.save
-        format.html { redirect_to post_url(@shared_post), notice: "Link shared!." }
+        format.html { redirect_to users_url(@shared_post), notice: "Link shared!." }
         format.json { render :show, status: :created, location: @shared_post }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @shared_post.errors, status: :unprocessable_entity }
       end
+    end
   end
 
   def edit
--- app/models/shared_post.rb
+++ app/models/shared_post.rb
@@ -2,10 +2,10 @@ class SharedPost < ApplicationRecord
   belongs_to :user
 
   def post_params(url)
-    post = LinkThumbnailer.generate("#{url}")
+    post = LinkThumbnailer.generate(url)
     title = post.title
     description = post.description
-    image = post.images.first.srs.to_s
+    image = post.images.first.src.to_s
     return { title: title, image_url: image, url: url }
   end
 end

05a08f95c45bb1114934ef61a1fa3cfbcc5d822d
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-24 11:48:11 -0700

    Adds methods to render all images in shared posts the same size, for now

--- app/helpers/shared_posts_helper.rb
+++ app/helpers/shared_posts_helper.rb
@@ -1,2 +1,11 @@
 module SharedPostsHelper
+  def post_image_for(shared_post, width = "", height = "")
+    @image_url = shared_post.image_url
+    if @image_url == nil
+      @image_shared_post = image_tag("user.jpg", alt: shared_post.description, width: width, height: height)
+    else
+      @image_shared_post = image_tag(shared_post.image_url, alt: shared_post.description, width: width, height: height)
+    end
+    return @image_shared_post
+  end
 end

1e88f3c3bf089f45752379d43d45923f11c543c8
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-24 08:49:01 -0700

    Adds routes and empty methods for shared_posts

--- app/controllers/shared_posts_controller.rb
+++ app/controllers/shared_posts_controller.rb
@@ -1,2 +1,22 @@
 class SharedPostsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, :original_posts, :relationships
+  resources :users, :original_posts, :relationships, :shared_posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

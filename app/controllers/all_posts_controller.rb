eb62a5cfe18759075a4b9bbf738df707f2fe1633
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-10-03 17:42:19 -0700

    Adds before_action's, private set methods, and index action to all_posts_controller

--- app/controllers/all_posts_controller.rb
+++ app/controllers/all_posts_controller.rb
@@ -1,2 +1,24 @@
 class AllPostsController < ApplicationController
+  before_action :set_original_post, only: %i[ show edit update destroy ]
+  before_action :set_shared_post, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[ new create edit update destroy ]
+
+  def index
+    @original_posts = OriginalPosts.all
+    @shared_posts = SharedPosts.all
+  end
+
+  private
+
+  def set_original_post
+    @original_post = OriginalPost.find(params[:id])
+  end
+
+  def set_shared_post
+    @shared_post = SharedPost.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user.id
+  end
 end

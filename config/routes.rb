13404ea5c88ed3f46c78a07e2e8b00538609e857
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-10-03 19:45:16 -0700

    Redefines root as all_posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  root "users#index"
+  root "all_posts#index"
   get "/login" => "sessions#new"
   post "/sessions" => "sessions#create"
   get "/logout" => "sessions#destroy"
